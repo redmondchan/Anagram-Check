80a2d488139e080fd2c19f406b50e7862c30bb55
Redmond Chan <redmondchan92@gmail.com>
2020-01-12 01:17:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let anagramCheck = (str1, str2) => {
+  if(str1.length != str2.length){
+    return false
+  }
+
+  let arr1 = str1.split("")
+  let arr2 = str2.split("")
+
+  for(let i = 0; i < arr1.length; i++){
+    let index = arr2.indexOf(`${arr1[i]}`)
+    if(index == -1){
+      return false
+    } else {
+      arr2.splice(`${index}`, 1)
+    }
+  }
+  return true
+}
+
+console.log(anagramCheck('abc', 'cba')) //true
+console.log(anagramCheck('ddvia', 'david')) //true
+console.log(anagramCheck('thisistrue', 'tehuirstis')) //true
+console.log(anagramCheck('aac', 'aab')) //false
+console.log(anagramCheck('dbvia', 'david')) //false
+console.log(anagramCheck('thisisfalse', 'tehsilsesfa')) //true
