cd5341c433a006d9b44ee690dbb447f8e4071560
Redmond Chan <redmondchan92@gmail.com>
2020-01-14 20:27:44 +0000

    added another method

--- index.js
+++ index.js
@@ -15,39 +15,66 @@
 //   return false
 // }
 
-// --------------method 3--------------------
+// --------------method 2--------------------
 let anagramCheck = (str1, str2) => {
-  
-  if(str1.length != str2.length){
+  if(str1.length !== str2.length){
     return false
   }
 
-  let obj1 = {}
-  let obj2 = {}
+  let obj = {}
 
   for(let char of str1){
-    if(obj1[char]){
-      obj1[char] += 1
+    if(obj[char]){
+      obj[char] +=1
     } else {
-      obj1[char]= 1
+      obj[char] = 1
     }
   }
 
   for(let char of str2){
-    if(obj2[char]){
-      obj2[char] += 1
-    } else {
-      obj2[char]= 1
-    }
-  }
-
-  for(let key in obj1){
-    if(obj1[key] !== obj2[key]){
+    //if obj doesn't have the letter or letter is zero it will return false
+    if(!obj[char]){
       return false
+    } else {
+      obj[char] -= 1
     }
   }
   return true
 }
+
+// --------------method 3--------------------
+// let anagramCheck = (str1, str2) => {
+  
+//   if(str1.length != str2.length){
+//     return false
+//   }
+
+//   let obj1 = {}
+//   let obj2 = {}
+
+//   for(let char of str1){
+//     if(obj1[char]){
+//       obj1[char] += 1
+//     } else {
+//       obj1[char]= 1
+//     }
+//   }
+
+//   for(let char of str2){
+//     if(obj2[char]){
+//       obj2[char] += 1
+//     } else {
+//       obj2[char]= 1
+//     }
+//   }
+
+//   for(let key in obj1){
+//     if(obj1[key] !== obj2[key]){
+//       return false
+//     }
+//   }
+//   return true
+// }
 
 // --------------method 4--------------------
 // let anagramCheck = (str1, str2) => {
